0b4a3dbab045a6531730441af4477c4350b61c64
sezginoztemir <eczaprodev@gmail.com>
2024-05-23 13:45:44 +0300

    initial

---
+++ paynamart.com/Client/Helper/SessionState.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Blazored.LocalStorage;
+using Microsoft.JSInterop;
+using System.Net;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace paynamart.com.Client.Helper
+{
+    public class SessionState
+    {
+        private readonly ILocalStorageService _local;
+        private readonly IJSRuntime _jsRuntime;
+        private static string SystemLang { get; set; } = "tr";
+        public SessionState(ILocalStorageService local, IJSRuntime jsRuntime)
+        {
+            _local = local;
+
+            _jsRuntime = jsRuntime;
+
+
+        }
+        public async Task SetLanguageLocal(string lang)
+        {
+            await _local.SetItemAsync<string>("lang", lang);
+
+        }
+        public async Task<string> GetLanguageLocal()
+        {
+            SystemLang = await _local.GetItemAsync<string>("lang");
+
+            return SystemLang;
+        }
+        public bool EmailIsValid(string email)
+        {
+            string expression = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+
+            if (Regex.IsMatch(email, expression))
+            {
+                if (Regex.Replace(email, expression, string.Empty).Length == 0)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+     
+
+    }
+}
+
---
+++ paynamart.com/Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System.IO.Compression;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.ResponseCompression;
+using paynamart.com.Client;
+using Blazored.LocalStorage;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+using paynamart.com.Client.Helper;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[] { "tr", "en" };
+    options.DefaultRequestCulture = new RequestCulture("tr");
+    options.AddSupportedCultures(supportedCultures);
+    options.AddSupportedUICultures(supportedCultures);
+});
+builder.Services.Configure<GzipCompressionProviderOptions>
+ (options => options.Level = CompressionLevel.Optimal);
+builder.Services.AddResponseCompression(options =>
+{
+    options.Providers.Add<GzipCompressionProvider>();
+});
+builder.Services.AddI18nText();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped<SessionState>();
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
+
